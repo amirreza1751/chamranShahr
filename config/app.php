31bd298dcd3bdbb44aa35902ae3768faf4981dd7
Amin Derakhshani <m.amin.derakhshani@gmail.com>
2019-09-15 19:24:40 +0430

    l5-swagger Install, key generate

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-
+        L5Swagger\L5SwaggerServiceProvider::class,
         /*
          * Package Service Providers...
          */
