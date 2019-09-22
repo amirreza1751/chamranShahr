1479a34022b82b0da610f311dff1ba6b1c18c7b1
amirreza <dashti.amir2752@gmail.com>
2019-09-22 04:39:00 -0700

    str replace commented

--- app/Console/Commands/NewsFetch.php
+++ app/Console/Commands/NewsFetch.php
@@ -52,8 +52,8 @@ class NewsFetch extends Command
                 'description' => $data->description,
             ];
 
-            $data->description = str_replace('<br>', '', $data->description);
-            $data->description = str_replace('&nbsp;', '', $data->description);
+//            $data->description = str_replace('<br>', '', $data->description);
+//            $data->description = str_replace('&nbsp;', '', $data->description);
 
             $check = News::where('link', $news['link'])->first();
             // find out that this news is a new one or not
@@ -66,8 +66,8 @@ class NewsFetch extends Command
             }
             else {
 
-                $check->description = str_replace('<br>', '', $check->description);
-                $check->description = str_replace('&nbsp;', '', $check->description);
+//                $check->description = str_replace('<br>', '', $check->description);
+//                $check->description = str_replace('&nbsp;', '', $check->description);
 
                 if(strcmp(substr(strval($data->description), -100), substr(strval($check->description), -100)) != 0){ // if is not but
                     $crawler = GoutteFacade::request('GET', 'http://scu.ac.ir/-/'.urlencode(str_replace('http://scu.ac.ir/-/', '',$news['link'])));
