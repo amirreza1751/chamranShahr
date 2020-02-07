d4c11e5e31168c13e73bc6794466878959aab5ed
amirreza <dashti.amir2752@gmail.com>
2020-02-07 15:58:36 -0800

    test bootstrap.js 6

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -63,7 +63,8 @@ window.Echo = new Echo({
     enabledTransports: ['ws', 'wss'],
     auth: {
         headers: {
-            Authorization: 'Bearer ' + window.axios.defaults.headers.common['X-CSRF-TOKEN']
+            // Authorization: 'Bearer ' + window.axios.defaults.headers.common['X-CSRF-TOKEN']
+            Authorization: window.axios.defaults.headers.common['X-CSRF-TOKEN']
         },
     }
 });
