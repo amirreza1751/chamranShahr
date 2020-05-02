f43d33c2b1c76f739266391877f551392596b2c3
Amin Derakhshani <m.amin.derakhshani@gmail.com>
2020-05-02 17:47:08 +0430

    debug SamaAuthController, third try

--- app/Console/Commands/Test.php
+++ app/Console/Commands/Test.php
@@ -38,6 +38,15 @@ class Test extends Command
      */
     public function handle()
     {
+        dump('scan path: ' . base_path() . "\\" . 'config');
+        print_r(scandir(base_path() . "\\" . 'config'));
+        if (is_dir('scan path: ' . base_path() . "\\" . 'config\cookie')){
+            dump('scan path: ' . base_path() . "\\" . 'config\\cookie');
+            print_r(scandir(base_path() . "\\" . 'config\\cookie'));
+        } else {
+            error_log('there is no cookie dir');
+        }
+        dump('now :');
         $login_result = app('App\Http\Controllers\API\Sama\SamaAuthController')->sama_login();
         if ($login_result){
             dump('true');
