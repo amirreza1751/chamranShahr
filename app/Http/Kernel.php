60ae2d85f24841fd9164c1e04217216b0049a97a
Amin Derakhshani <m.amin.derakhshani@gmail.com>
2020-11-07 22:42:16 +0330

    update swagger route security

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'cors' => \App\Http\Middleware\Cors::class,
+        'docs' => \App\Http\Middleware\SecureApiDocs::class,
     ];
 
 }
